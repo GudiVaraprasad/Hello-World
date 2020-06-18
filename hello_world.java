1a8d8c06fcb518aacb9f82c3a21397f40b76673a
Gudi Varaprasad <53944466+GudiVaraprasad@users.noreply.github.com>
2020-06-18 13:07:56 +0530

    Create hello_world.java

---
+++ hello_world.java
@@ -0,0 +1,12 @@
+// Hello world programme in Java
+
+public class hello_world
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello world");
+	}
+}
+
+
+// Output : Hello world
